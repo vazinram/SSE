b31406d541d21bd647b23f7a2191135759992abc
DavidWenger1 <davidwenger130@gmail.com>
2022-09-08 15:09:00 +0200

    Square Func implemented

--- calc.py
+++ calc.py
@@ -3,19 +3,19 @@ y = int(input("please enter number two for ADD:"))
 z = int(input("please define Sum, MULTIPLY, dIVIDING, Subtract by 1, 2, 3 and 4"))
 #Basic 2-number Calculator Functions
 def add(x,y):
-	return x + y
+	return x+y
 
 def multiply(x,y):
 	return x*y
 
 def subtract(x,y):
-	return(x - y)
+	return x-y
 
 def divide(x,y):
 	return x/y
 
 def square(x,y):
-    return x ** y
+    return x**y
     
 if z==1:
     print(add(x,y))
@@ -25,3 +25,5 @@ elif z == 3:
     print(subtract(x,y))
 elif z==4:
     print(divide(x,y))
+elif z==5:
+    print(square(x,y)) 
