f46d112935153fa44b19695d0d1960a9aaab966d
vazinram <109950796+vazinram@users.noreply.github.com>
2022-09-08 14:37:24 +0200

    new functions

--- calc.py
+++ calc.py
@@ -1,16 +1,26 @@
 x = int(input("please enter number one for ADD:"))
 y = int(input("please enter number two for ADD:"))
-
+z = int(input("please define Sum, MULTIPLY, dIVIDING, Subtract by 1, 2, 3 and 4"))
 #Basic 2-number Calculator Functions
+
+
 def add(x,y):
-	pass
+    return(x+y)
 
 def multiply(x,y):
 	return x*y
 
 def subtract(x,y):
-	pass
+	return(x - y)
 
 def divide(x,y):
-	return x//y
+	return x/y
     
+if z==1:
+    print(add(x,y))
+elif z==2:
+    print(multiply(x,y))
+elif z == 3:
+    print(subtract(x,y))
+elif z==4:
+    print(divide(x,y))
